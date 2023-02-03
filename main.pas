4d13357814360dd840e70c7e6d5a825964af34e2
terminal <m.terlyck@yandex.ru>
2023-02-03 23:09:27 +0300

    Added execution for tsGraphic

--- main.pas
+++ main.pas
@@ -2,14 +2,8 @@ program TrapsArea;
 
 { 1*x^3 + 1*x^2 + (-4)*x + 13 }
 
-{
-// График
-- [ ] Создать буферизацию графиков
-- [ ] Добавить окошко клавиш
-}
-
 uses
-  Crt, ptcGraph, ptcCrt, tsCalc;
+  Crt, SysUtils, tsCalc;
 
 const
   menu: array[1..6] of string = (
@@ -22,8 +16,7 @@ const
   xRoot = -3.35;
 var
   i: integer;
-  selectedItem, scaleX, scaleY: byte;
-  n: word;
+  selectedItem: byte;
   userKey: char;
 
 
@@ -134,213 +127,11 @@ var
     primitivef := xr * xr * xr * xr / 4 + xr * xr * xr / 3 - xr * xr * 2 + 13 * xr;
   end;
 
-  function strF(s: real): string;
-  begin
-    str(s: 2: 2, strF);
-  end;
-
-  function border(x, br: integer): integer;
-  begin
-    if x < br then
-      border := x
-    else
-      border := br;
-  end;
-
-  //procedure trapArea; forward;
-
-  //procedure graphX(scale);
-
   procedure graphWindow; {Графика}
-  var
-    x, y: longint;
-    xn, yn, x0, y0: word;
-    Driver, Mode: smallint;
-    xReal, Si: currency;
-    userValue, lblText: string;
-    pFrame: Pointer;
-    size: integer;
   begin
     newPage(menu[selectedItem]);
-
-    scaleX := 15;
-    scaleY := 10;
-
-    Driver:=10;
-    Mode:=260;
-    initGraph(Driver, Mode, '');
-
-    i := GraphResult; {Запоминаем результат}
-    if i <> grOk then {Проверяем ошибку}
-    begin
-      writeln(GraphErrorMsg(i));
-      readKey;
-    end;
-
-    xn := GetMaxX - 20;
-    yn := GetMaxY - 20;
-    x0 := xn div 2;
-    y0 := yn div 5 * 4;
-
-    repeat
-      ClearDevice;
-      setColor(White);
-      rectangle(20, 20, xn, yn);
-      outTextXY(x0 - 2, 4, 'Y');
-      outTextXY(xn + 4, y0 - 2, 'X');
-
-      line(20, y0, xn, y0);
-      line(x0, 20, x0, yn);
-
-{       Size := ImageSize(20, 20, xn, yn);
-      GetMem(pFrame, Size);
-      GetImage(20, 20, xn, yn, pFrame^); }
-
-      xReal := (xn - 20) / 10;
-      for i := -4 to 4 do
-      begin
-        x := trunc(i * xReal);
-        line(x0 + x, y0, x0 + x, y0 + 5);
-        str(x / scaleX: 1: 1, userValue);
-        outTextXY(x0 + x, y0 + 6, userValue);
-      end;
-      xReal := (yn - 20) / 12;
-      for i := 1 to 10 do
-      begin
-        y := trunc(i * xReal);
-        line(x0, y0 - y, x0 + 5, y0 - y);
-        str(y / scaleY: 1: 1, userValue);
-        outTextXY(x0 + 6, y0 - y, userValue);
-      end;
-
-      setColor(Cyan);
-      y := yn - 1;
-      moveTo(20, y);
-      for x := x0 - xn + 22 to xn - x0 do
-      begin
-        y := y0 - trunc(f(x / scaleX) * scaleY);
-        if 20 > y then
-          y := 21;
-        if y > yn then
-          y := yn - 1;
-        lineTo(x0 + x, y);
-      end;
-
-      if status then
-      begin
-        x := x0 + trunc(scaleX * a);
-        if x <= xn then
-        begin
-          moveTo(x, 21);
-          lineTo(x, y0 - 1);
-          y := border(x0 + trunc(scaleX * b), xn);
-          if x + 1 < y then
-          begin
-            lineTo(y, y0 - 1);
-            lineTo(y, 21);
-            setFillStyle(3, Blue);
-            floodFill(y - 1, y0 - 2, Cyan);
-            outTextXY(30, 30, 'Limits: ' + strF(a) + ', ' + strF(b));
-          end;
-        end;
-      end
-      else
-        outTextXY(30, 30, 'Limits are empty');
-
-      if status then
-      begin
-        setColor(Red);
-        xReal := a;
-        area := 0;
-        setFillStyle(1, Black);
-        for i := 1 to n do
-        begin
-          moveTo(x0 + trunc(xReal * scaleX), y0);
-          y := y0 - trunc(f(xReal) * scaleY);
-          x := border(x0 + trunc(xReal * scaleX), xn);
-          if y > yn then
-            moveTo(x, yn - 1)
-          else if 20 > y then
-            moveTo(x, 21)
-          else
-            lineTo(x, y);
-
-
-          xReal := xReal + h;
-          y := y0 - trunc(f(xReal) * scaleY);
-          x := border(x0 + trunc(xReal * scaleX), xn);
-          if y > yn then
-            moveTo(x, yn - 1)
-          else if 20 > y then
-            moveTo(x, 21)
-          else
-            lineTo(x, y);
-          lineTo(x, y0);
-          bar(30, 40, 220, 60);
-          Si := (f(xReal) + f(xReal - h)) / 2 * h;
-          str(i, lblText);
-          outTextXY(30, 40, 'S' + lblText + ' = ' + strF(Si));
-          delay(1000);
-        end;
-
-        delay(2000);
-        if not complite then
-          trapArea;
-        outTextXY(30, 60, 'S = ' + strF(area));
-        complite := True;
-      end;
-      //writeln;
-      userKey := ptcCrt.readkey;
-      case userKey of
-        #0: 
-        begin
-          userKey := ptcCrt.readKey;
-          case userKey of
-            #80:
-            begin
-              Dec(scaleY);
-              if scaleY = 0 then
-                scaleY := 1;
-            end;
-            #72: Inc(scaleY);
-            #75:
-            begin
-              Dec(scaleX);
-              if scaleX = 0 then
-                scaleX := 1;
-            end;
-            #77: Inc(scaleX);
-          end;
-        end;
-        #116: PutImage(100, 100, pFrame^, NormalPut);
-        #27:
-        begin
-          closeGraph;
-          restoreCrtMode;
-          break;
-        end;
-      end;
-      // freeMem(pFrame, size);
-    until False;
+    ExecuteProcess('./tsGraphic',['arg1','arg2']);
   end;
-
-  // procedure trapArea;
-  // { Непосредственно вычисление самого алгоитма и вывод}
-  // var
-  //   x: real;
-  // begin
-  //   newPage(menu[selectedItem]);
-  //   x := a + h;
-  //   area := (f(a) + f(b)) / 2;
-  //   while (b - x) > t do
-  //   begin
-  //     area := area + f(x);
-  //     x := x + h;
-  //   end;
-  //   area := area * h;
-  //   writeln('Area = ', area: 2: 2);
-  //   complite := True;
-  // end;
 
   procedure errorCalculate;
   var
@@ -374,7 +165,6 @@ var
   procedure quit;
   begin
     clrScr;
-    closeGraph;
     halt(0);
   end;
 
@@ -443,6 +233,5 @@ var
   end;
 
 begin
-  status := False;
   TUImenu;
 end.
