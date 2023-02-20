51af4fc0f8f7f3890973bfa20fe18062e7f04d83
terminal <m.terlyck@yandex.ru>
2023-02-20 09:26:32 +0300

    Little optimize

--- tsGraphic.pas
+++ tsGraphic.pas
@@ -18,10 +18,10 @@ var
   x, y, fSize: longint;
   xReal, Si: currency;
   userKey: char;
-  {lblText, }userValue: string;
+  userValue: string;
   pFrame: Pointer;
 
-  function border(x, br: integer): integer;
+function border(x, br: integer): integer;
   begin
     if x < br then
       border := x
@@ -29,49 +29,127 @@ var
       border := br;
   end;
 
+procedure drawArea;
+begin
+  x := x0 + trunc(scaleX * a);
+  if x <= xn then
+  begin
+	if x < 21 then
+	  x := 21;
+	moveTo(x, 21);
+	lineTo(x, y0 - 1);
+	y := border(x0 + trunc(scaleX * b), xn-1);
+	if x + 1 < y then
+	begin
+	  lineTo(y, y0 - 1);
+	  lineTo(y, 21);
+	  setFillStyle(3, Blue);
+	  floodFill(y - 1, y0 - 2, Cyan);
+	end;
+  end;
+end;
+
+procedure drawAbout;
+begin
+  setFillStyle(1, White);
+  setColor(Black);
+  bar(25, 25, xn-5, 110);
+  outTextXY(30, 30, 'TrapsArea is a programm for calculation of an area of figure');
+  outTextXY(30, 45, 'limited by the Ox axis and curve');
+  outTextXY(30, 60, Description);
+  outTextXY(30, 75, 'using the Trapezoid method.');
+  outTextXY(30, 90, 'Made by Terlyck Maxim');
+  ptcCrt.readKey;
+end;
+
+procedure drawIntegral;
+  begin
+	setFillStyle(1, White);
+	setColor(Black);
+	outTextXY(30, 60, '[Enter] to continue');
+	xReal := a;
+	area := 0;
+	for i := 1 to n do
+	begin
+	  setColor(Magenta);
+	  moveTo(x0 + trunc(xReal * scaleX), y0);
+	  y := y0 - trunc(f(xReal) * scaleY);
+	  x := border(x0 + trunc(xReal * scaleX), xn);
+	  if y > yn then
+		moveTo(x, yn - 1)
+	  else if 20 > y then
+		moveTo(x, 21)
+	  else
+		lineTo(x, y);
+
+
+	  xReal := xReal + h;
+	  y := y0 - trunc(f(xReal) * scaleY);
+	  x := border(x0 + trunc(xReal * scaleX), xn);
+	  if y > yn then
+		moveTo(x, yn - 1)
+	  else if 20 > y then
+		moveTo(x, 21)
+	  else
+		lineTo(x, y);
+	  lineTo(x, y0);
+	  bar(30, 40, 200, 55);
+	  Si := (f(xReal) + f(xReal - h)) / 2 * h;
+	  setColor(Black);
+	  outTextXY(30, 40, 'S' + strFi(i) + ' = ' + strF(Si));
+	  userKey := ptcCrt.readkey;
+	  if userKey = #27 then
+		break;
+	end;
+	bar(30, 60, 200, 80);
+	trapArea;
+	outTextXY(30, 60, 'S = ' + strF(area));
+
+	ptcCrt.readKey;
+  end;
+
 begin
 
-  if paramCount <> 0 then
+  if paramCount > 0 then
     case paramStr(1) of
-    '-li', '-lic': begin
-      if paramCount > 3 then begin
-        errorCount := 0;
-        val(paramStr(2), a, i);
-        if i <> 0 then begin
-          writeLn('Error in the "a" parameter: ', paramStr(2));
-          inc(errorCount);
-        end;
-        val(paramStr(3), b, i);
-        if i <> 0 then begin
-          writeLn('Error in the "b" parameter: ', paramStr(3));
-          inc(errorCount);
-        end;
-        val(paramStr(4), n, i);
-        if (i <> 0) or (n = 0) then begin
-          writeLn('Error in the "n" parameter: ', paramStr(4));
-          inc(errorCount);
-        end;
+    '-li', '-lic': 
+	  begin
+		errorCount := 0;
+		val(paramStr(2), a, i);
+		if i <> 0 then begin
+		  writeLn('Error in the "a" parameter: ', paramStr(2));
+		  inc(errorCount);
+		end;
+		val(paramStr(3), b, i);
+		if i <> 0 then begin
+		  writeLn('Error in the "b" parameter: ', paramStr(3));
+		  inc(errorCount);
+		end;
+		val(paramStr(4), n, i);
+		if (i <> 0) or (n = 0) then begin
+		  writeLn('Error in the "n" parameter: ', paramStr(4));
+		  inc(errorCount);
+		end;
 
-        if errorCount = 0 then begin
-          if paramStr(1) = '-li' then begin
-            if a < xRoot then
-              a := xRoot;
-            if b < xRoot then
-              b := xRoot;
-            if a > b then begin
-              a := a + b;
-              b := a - b;
-              a := a - b;
-            end;
-          end;
-          h := (b - a) / n;
-          status := True;
-        end
-      else
-        writeLn('Not enough parameters');
+		if errorCount = 0 then begin
+		  if paramStr(1) = '-li' then begin
+			if a < xRoot then
+			  a := xRoot;
+			if b < xRoot then
+			  b := xRoot;
+			if a > b then begin
+			  a := a + b;
+			  b := a - b;
+			  a := a - b;
+			end;
+		  end;
+		  h := (b - a) / n;
+		  status := True;
+		end
+		else
+		  writeLn('Error of child parameters');
+      end;
     end;
-    end;
-  end;
 
   scaleX := 15;
   scaleY := 10;
@@ -135,24 +213,7 @@ begin
     end;
 
     if status then
-    begin
-      x := x0 + trunc(scaleX * a);
-	  if x <= xn then
-      begin
-		if x < 21 then
-		  x := 21;
-        moveTo(x, 21);
-        lineTo(x, y0 - 1);
-        y := border(x0 + trunc(scaleX * b), xn-1);
-        if x + 1 < y then
-        begin
-          lineTo(y, y0 - 1);
-          lineTo(y, 21);
-          setFillStyle(3, Blue);
-          floodFill(y - 1, y0 - 2, Cyan);
-        end;
-      end;
-    end;
+      drawArea;
 
     setColor(White);
     line(x0, 20, x0, yn);
@@ -183,79 +244,20 @@ begin
         case userKey of
           #80:
           begin
-            Dec(scaleY);
-            if scaleY = 0 then
-              scaleY := 1;
+            if scaleY > 1 then
+			  Dec(scaleY);
           end;
           #72: 
           if scaleY div scaleX < 16 then
             Inc(scaleY);
           #75:
-          begin
-            if (scaleY div scaleX < 16) and (scaleX >= 1) then
-              Dec(scaleX);
-          end;
+          if (scaleY div scaleX < 16) and (scaleX > 1) then
+            Dec(scaleX);
           #77: Inc(scaleX);
         end;
       end;
-	  #97, #228:
-	  begin
-		setFillStyle(1, White);
-		setColor(Black);
-		bar(25, 25, xn-5, 110);
-		outTextXY(30, 30, 'TrapsArea is a programm for calculation of an area of figure');
-		outTextXY(30, 45, 'limited by the Ox axis and curve');
-		outTextXY(30, 60, Description);
-		outTextXY(30, 75, 'using the Trapezoid method.');
-		outTextXY(30, 90, 'Made by Terlyck Maxim');
-		ptcCrt.readKey;
-	  end;
-      'i', #248:
-      if status then
-      begin
-        setFillStyle(1, White);
-        setColor(Black);
-        outTextXY(30, 60, '[Enter] to continue');
-        xReal := a;
-        area := 0;
-        for i := 1 to n do
-        begin
-          setColor(Magenta);
-          moveTo(x0 + trunc(xReal * scaleX), y0);
-          y := y0 - trunc(f(xReal) * scaleY);
-          x := border(x0 + trunc(xReal * scaleX), xn);
-          if y > yn then
-            moveTo(x, yn - 1)
-          else if 20 > y then
-            moveTo(x, 21)
-          else
-            lineTo(x, y);
-
-
-          xReal := xReal + h;
-          y := y0 - trunc(f(xReal) * scaleY);
-          x := border(x0 + trunc(xReal * scaleX), xn);
-          if y > yn then
-            moveTo(x, yn - 1)
-          else if 20 > y then
-            moveTo(x, 21)
-          else
-            lineTo(x, y);
-          lineTo(x, y0);
-          bar(30, 40, 200, 55);
-          Si := (f(xReal) + f(xReal - h)) / 2 * h;
-          setColor(Black);
-          outTextXY(30, 40, 'S' + strFi(i) + ' = ' + strF(Si));
-          userKey := ptcCrt.readkey;
-          if userKey = #27 then
-            break;
-        end;
-        bar(30, 60, 200, 80);
-        trapArea;
-        outTextXY(30, 60, 'S = ' + strF(area));
-
-        ptcCrt.readKey;
-      end;
+	  #97, #228: drawAbout;
+      'i', #248: if status then drawIntegral;
       #27:
       begin
         restoreCrtMode;
